e38969aabb2beafd6fd1c0aa61fa6c830b8f88bf
Josh Bebbington <josh@focalwebsites.com>
2015-01-05 09:01:35 +0000

    Setup initial sqlite database with an item

--- page.rb
+++ page.rb
@@ -1,9 +1,19 @@
 require 'sinatra'
+require 'data_mapper'
 
-get '/' do
-  'Hello world!'
+# Setup connection to database
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/todo_list.db")
+
+
+# Create basic 'item' database model.
+class Item
+  include DataMapper::Resource
+  property :id, Serial
+  property :content, Text, :required => true
+  property :done, Boolean, :required => true, :default => false
+  property :created, DateTime
 end
 
-get '/hello/:name' do
-  "Hello #{params['name']}"
-end
+# Create database.
+DataMapper.finalize.auto_upgrade!
+
